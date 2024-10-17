8d628525ee53191b6811423731efd71d5c8b3960
Oren <orenz0@protonmail.com>
2024-10-18 00:54:23 +0300

    fix stream, convert text

--- api/png.ts
+++ api/png.ts
@@ -1,6 +1,20 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
 import text2png from 'text2png';
 
+function toMultiline(text: string, breakLength: number = 22): string {
+  let result = '';
+  let lineLength = 0;
+  for (const word of text.split(' ')) {
+    if (lineLength >= breakLength) {
+      result += '\n';
+      lineLength = 0;
+    }
+    result += ` ${word}`;
+    lineLength += word.length + 1;
+  }
+  return result;
+}
+
 export default function handler(req: VercelRequest, res: VercelResponse) {
   const text = [
     `Country: ${req.headers['x-vercel-ip-country']}`,
@@ -10,8 +24,15 @@ export default function handler(req: VercelRequest, res: VercelResponse) {
     `Lat: ${req.headers['x-vercel-ip-latitude']} Lng: ${req.headers['x-vercel-ip-longitude']}`,
     `User-Agent: ${req.headers['user-agent']}`,
     `IP: ${req.headers['x-real-ip']}`,
-  ].join('\n');
-  const pngStream = text2png(text, { color: 'purple' });
+  ].map(line => toMultiline(line)).join('\n');
+  const pngStream = text2png(text, {
+    color: 'purple',
+    backgroundColor: 'linen',
+    font: '80px Futura',
+    lineSpacing: 10,
+    padding: 100,
+    output: 'stream',
+  });
   res.setHeader('content-type', 'image/png');
-  pngStream.pipe(res);
+  pngStream.pipe(res)
 }
