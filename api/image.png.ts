89a73509b9a3392e8a60fa9687861ffdced1f34f
cxplay <62034099+cxplay@users.noreply.github.com>
2024-10-22 02:38:17 +0800

    first custom

--- api/image.png.ts
+++ api/image.png.ts
@@ -18,23 +18,30 @@ function toMultiline(text: string, breakLength: number = 22): string {
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
   const text = [
-    `Country: ${req.headers['x-vercel-ip-country']}`,
-    `Region (ISO 3166-2): ${req.headers['x-vercel-ip-country-region']}`,
-    `City: ${req.headers['x-vercel-ip-city']}`,
-    `Timezone: ${req.headers['x-vercel-ip-timezone']}`,
-    `Lat: ${req.headers['x-vercel-ip-latitude']} Lng: ${req.headers['x-vercel-ip-longitude']}`,
-    `User-Agent: ${req.headers['user-agent']}`,
-    `IP: ${req.headers['x-real-ip']}`,
+    `• IP ············· ${req.headers['x-real-ip']}`,
+    `• Region ········· ${req.headers['x-vercel-ip-country-region']}`,
+    `• City ··········· ${req.headers['x-vercel-ip-city']}`,
+    `• Country ········ ${req.headers['x-vercel-ip-country']}`,
+    `• Lat/Lng ········ ${req.headers['x-vercel-ip-latitude']}/${req.headers['x-vercel-ip-longitude']}`,
+    `• Timezone ······· ${req.headers['x-vercel-ip-timezone']}`,
+    `• Language ······· ${req.headers['accept-language']}`,
+    `• Encoding ······· ${req.headers['accept-encoding']}`,
+    `• Accept ········· ${req.headers['accept']}`,
+    `• Cache ·········· ${req.headers['cache-control']}`,
+    `• User-Agent ····· ${req.headers['user-agent']}`,
   ].map(line => toMultiline(line)).join('\n');
   const pngStream = text2png(text, {
-    localFontPath: path.join(__dirname, '..', 'Arial.ttf'),
-    localFontName: 'Arial',
-    color: 'purple',
-    backgroundColor: 'linen',
-    font: '20px Arial',
+    localFontPath: path.join(__dirname, '..', 'UbuntuMono-R.ttf'),
+    localFontName: 'UbuntuMono',
+    color: 'green',
+    backgroundColor: 'black',
+    font: '28px UbuntuMono',
+    textAlign: 'left',
     lineSpacing: 8,
     padding: 25,
     output: 'stream',
+    borderLeftWidth: 100,
+    borderRightWidth: 100
   });
   res.setHeader('content-type', 'image/png');
   pngStream.pipe(res)
