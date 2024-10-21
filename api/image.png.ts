5ece0ba27eca75957a9f3df1da27ce41bf455c20
cxplay <62034099+cxplay@users.noreply.github.com>
2024-10-22 03:43:09 +0800

    add header and tweak style

--- api/image.png.ts
+++ api/image.png.ts
@@ -17,18 +17,30 @@ function toMultiline(text: string, breakLength: number = 22): string {
 }
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
+  const userAgent = req.headers['user-agent']
+    ? req.headers['user-agent']
+        .replace(/ /g, '·')
+        .replace(/([^\s]{32})/g, '\n───────────────────╢$1')
+    : 'none';
+  const acceptEncoding = req.headers['accept-encoding']
+    ? req.headers['accept-encoding'].replace(/ /g, '·')
+    : 'none';
+  const accept = req.headers['accept']
+    ? req.headers['accept'].replace(/([^\s]{32})/g, '\n───────────────────╢$1')
+    : 'none';
   const text = [
     `• IP ············· ${req.headers['x-real-ip']}`,
+    `• ASN ············ ${req.headers['x-vercel-ip-as-number']}`,
     `• Region ········· ${req.headers['x-vercel-ip-country-region']}`,
     `• City ··········· ${req.headers['x-vercel-ip-city']}`,
     `• Country ········ ${req.headers['x-vercel-ip-country']}`,
     `• Lat/Lng ········ ${req.headers['x-vercel-ip-latitude']}/${req.headers['x-vercel-ip-longitude']}`,
     `• Timezone ······· ${req.headers['x-vercel-ip-timezone']}`,
     `• Language ······· ${req.headers['accept-language']}`,
-    `• Encoding ······· ${req.headers['accept-encoding']}`,
-    `• Accept ········· ${req.headers['accept']}`,
     `• Cache ·········· ${req.headers['cache-control']}`,
-    `• User-Agent ····· ${req.headers['user-agent']}`,
+    `• Encoding ······· ${acceptEncoding}`,
+    `• Accept ········· ${accept}`,
+    `• User-Agent ····· ${userAgent}`,
   ].map(line => toMultiline(line)).join('\n');
   const pngStream = text2png(text, {
     localFontPath: path.join(__dirname, '..', 'UbuntuMono-R.ttf'),
